c36cabc4d806a7091d9a8cc3b202e0ef6311e476
Your Name <15144467166>
2018-04-12 19:51:03 +0800

    gaoxin

---
+++ project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+
+import AppHeader from './components/Header'
+
+export default {
+  name: 'App',
+  components:{ AppHeader }
+
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ project/src/components/Header.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="header">
+    <img src="../assets/imgs/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name:'AppHeader',
+  data () {
+      return {
+          data: ''
+      }
+  }
+}
+</script>
+<style scoped>
+.header{
+    background:#2ca2f9;
+    width:100%;
+    height:.6rem;
+}
+   
+        
+
+</style>
+
